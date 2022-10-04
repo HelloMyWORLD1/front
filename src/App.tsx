d400b59ebe3ca895d4f5e132067ad17132fcb8ae
JaewonyH <ghkd6535@naver.com>
2022-10-04 14:43:25 +0900

    Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage"
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
 
 //Route 설정.
 
 const App = () => {
-    return <div>test</div>;
+    return (
+        <Routes>
+            {/* 홈페이지 */}
+            <Route path="/" element={<HomePage />} />
+
+            {/* 로그인,회원가입 페이지 */}
+            <Route path="/login" element={<LogInPage />} />
+            <Route path="/login/sign_up" element={<SignUpPage />} />
+        </Routes> 
+    );
 };
 
 export default App;
