8ff21b14a6d8fa5e5de4271c74f0d6ba0b5a843a
JaewonyH <ghkd6535@naver.com>
2022-10-12 17:31:58 +0900

    portFolio 비동기 처리

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,15 +2,19 @@
 
 interface UserInitialState {
     user: null | object;
+    //로그인
     logInLoading: boolean;
     logInDone: boolean;
     logInError: null | object;
+    //로그아웃
     logOutLoading: boolean;
     logOutDone: boolean;
     logOutError: null | object;
+    //회원가입
     signUpLoading: boolean;
     signUpDone: boolean;
     signUpError: null | object;
+
     findPasswordLoading: boolean;
     findPasswordDone: boolean;
     findPasswordError: null | object;
@@ -27,10 +31,28 @@ interface UserInitialState {
 
 interface PortFolioInitalState {
     portFolio: null | object;
+    //개인 포폴 등록
     registerPortFolioLoading: boolean;
     registerPortFolioDone: boolean;
     registerPortFolioError: null | object;
+    //개인 포폴 조회
     getPortFolioLoading: boolean;
     getPortFolioDone: boolean;
     getPortFolioError: null | object;
+    //분야별(좋아요순) 포폴 조회
+    getPortFolioLikeLoading: boolean;
+    getPortFolioLikeDone: boolean;
+    getPortFoiloLikeError: null | object;
+    //분야별(최신순) 포폴 조회
+    getPortFolioLatestLoading: boolean;
+    getPortFolioLatestDone : boolean;
+    getPortFolioLatestError: null | object;
+    //포폴 팔로우하기
+    followPortFolioLoading: boolean;
+    followPortFolioDone: boolean;
+    followPortFolioError: null | object;
+    //포폴 팔로우 취소
+    unFollowPortFolioLoading: boolean;
+    unFollowPortFolioDone: boolean;
+    unFollowPortFolioError: null | object;
 }
