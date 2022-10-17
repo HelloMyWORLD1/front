0424f2f8dca74c2ffb4013619ab137829a33223f
JaewonyH <ghkd6535@naver.com>
2022-10-17 14:38:14 +0900

    로그인,회원가입 퍼블리싱

--- src/components/styled.tsx
+++ src/components/styled.tsx
@@ -488,20 +488,73 @@ text-transform: uppercase;
 
 color: #999999;
 `;
-export const Mask = styled.img``;
+export const Mask = styled.img`
+position: absolute;
+width: 150px;
+height: 150px;
+left: 885px;
+top: 390px;
+`;
 export const UploadImg = styled.img`
-// position: absolute;
-// width: 40px;
-// height: 40px;
-// left: 940px;
-// top: 435px;
+position: absolute;
+width: 40px;
+height: 40px;
+left: 940px;
+top: 435px;
 `;
 export const UploadBtn = styled.button`
-// position: absolute;
-// width: 78px;
-// height: 60px;
-// left: 921px;
-// top: 435px;
+border: 0;
+outline: 0;
+background-color : rgba(0,0,0,0);
+position: absolute;
+width: 78px;
+height: 60px;
+left: 921px;
+top: 435px;
+`;
+export const NicknameInput = styled.input`
+box-sizing: border-box;
+
+position: absolute;
+width: 500px;
+height: 60px;
+left: 710px;
+top: 570px;
+
+background: #FFFFFF;
+border: 1px solid #E5E5E5;
+
+font-family: 'Noto Sans CJK KR';
+font-style: normal;
+font-weight: 350;
+font-size: 14px;
+line-height: 20px;
+letter-spacing: -0.1em;
+&::placeholder{
+  color: #999999;
+}
+
+`;
+export const RegisterProfileBtn = styled.button`
+position: absolute;
+width: 502px;
+height: 60px;
+left: 710px;
+top: 640px;
+
+background: #222222;
+font-family: 'Noto Sans CJK KR';
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 26px;
+/* identical to box height */
+
+text-align: center;
+letter-spacing: -0.1em;
+
+color: #FFFFFF;
+
 `;
 
 
--- src/pages/SignUpProfilePage.tsx
+++ src/pages/SignUpProfilePage.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import SignUpProfileForm from "../components/SignUpProfileForm";
 import { LogoImg, SignUpProfileTxt,SignUpProfileTxt2 } from "../components/styled";
 import logo from "../img/logo.png";
+import { useNavigate } from "react-router-dom";
+
 export default function SignUpProfilePage() {
+  const navigate = useNavigate();
+        const gotoHome = () => {
+            navigate('/');
+        };
   return (
     <div>
       <SignUpProfileTxt>프로필등록</SignUpProfileTxt>
       <SignUpProfileTxt2>사용하실 별명과 사진을 등록해주세요</SignUpProfileTxt2>
-      <LogoImg src={logo}></LogoImg>
+      <LogoImg src={logo} onClick={gotoHome} ></LogoImg>
       <SignUpProfileForm />
     </div>
   );
