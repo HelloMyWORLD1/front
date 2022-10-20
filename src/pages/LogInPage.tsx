acea1637c1d46b83be397092a99be097e655abec
leeuihyun <meta212121@naver.com>
2022-10-20 21:03:17 +0900

    logInPage 수정 예정

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
-import { LogoImg, SignupTag,FingTag, AutoLoginTxt , AutoLoginImg} from "../components/styled";
-import logo from "../img/logo.png"
-import circleFill from "../img/akar-icons_circle-check-fill.svg"
+import {
+    LogoImg,
+    SignupTag,
+    FingTag,
+    AutoLoginTxt,
+    AutoLoginImg,
+} from "../components/styled";
+import logo from "../img/logo.png";
+import circleFill from "../img/akar-icons_circle-check-fill.svg";
 import { useNavigate } from "react-router-dom";
-
 
 function LogInPage() {
     const navigate = useNavigate();
-        const gotoHome = () => {
-            navigate('/');
-        };
-        
+    const gotoHome = () => {
+        navigate("/");
+    };
+
     return (
         <div>
-            <LogoImg src={logo} onClick = {gotoHome}></LogoImg>
+            <LogoImg src={logo} onClick={gotoHome}></LogoImg>
             <LoginForm />
             <SignupTag href="/signUp">회원가입</SignupTag>
             <FingTag href="#">아이디 ∙ 비밀번호 찾기</FingTag>
