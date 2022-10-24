c61369285ea3ff81faaa7711acab5cd39c9ae998
leeuihyun <meta212121@naver.com>
2022-10-24 18:57:52 +0900

    Components

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,171 +1,166 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
-    EmailSignUpInput,
-    EmailSignUpSelect,
-    BirthInput,
-    PhoneInput,
-    NameInput,
-    PwCheckInput,
-    PwSignUpInput,
-    SignUpBtn,
-    NameTxt,
-    BirthTxt,
-    PhoneTxt,
-    EmailTxt,
-    PwTxt,
-    PwCheckTxt,
-    SignUpComponent,
-    SignUpTxt,
-    SignUpTxt2,
-    SignUpInsideBox,
-    LogoImg,
-    BlackLine,
-    ExplainTxt,
+  EmailSignUpInput,
+  EmailSignUpSelect,
+  BirthInput,
+  PhoneInput,
+  NameInput,
+  PwCheckInput,
+  PwSignUpInput,
+  SignUpBtn,
+  NameTxt,
+  BirthTxt,
+  PhoneTxt,
+  EmailTxt,
+  PwTxt,
+  PwCheckTxt,
+  SignUpComponent,
+  SignUpTxt,
+  SignUpTxt2,
+  SignUpInsideBox,
+  LogoImg,
+  BlackLine,
+  ExplainTxt,
 } from "./styled";
 import logo from "../img/logo.png";
 
 function SignUpForm() {
-    const [name, setName] = useState<string>("");
-    const [birth, setBirth] = useState<number>();
-    const [phone, setPhone] = useState<number>();
-    const [email, setEmail] = useState<string>("");
-    const [pw, setPw] = useState<string>("");
-    const [pwCheck, setPwCheck] = useState<string>("");
-    const [domain, setDomain] = useState<string>("");
-    const selectList = [
-        "도메인 선택",
-        "@naver.com",
-        "hanmail.net",
-        "daum.net",
-        "nate.com",
-        "gmail.com",
-        "hotmail.com",
-        "lycos.co.kr",
-        "empal.com",
-        "cyworld.com",
-        "yahoo.com",
-        "paran.com",
-        "dreamwiz.com",
-        "직접입력",
-    ];
+  const [name, setName] = useState<string>("");
+  const [birth, setBirth] = useState<number>();
+  const [phone, setPhone] = useState<number>();
+  const [email, setEmail] = useState<string>("");
+  const [pw, setPw] = useState<string>("");
+  const [pwCheck, setPwCheck] = useState<string>("");
+  const [domain, setDomain] = useState<string>("");
+  const selectList = [
+    "도메인 선택",
+    "@naver.com",
+    "hanmail.net",
+    "daum.net",
+    "nate.com",
+    "gmail.com",
+    "hotmail.com",
+    "lycos.co.kr",
+    "empal.com",
+    "cyworld.com",
+    "yahoo.com",
+    "paran.com",
+    "dreamwiz.com",
+    "직접입력",
+  ];
 
-    const navigate = useNavigate();
-    const gotoHome = () => {
-        navigate("/");
-    };
-    const gotoProfile = () => {
-        navigate("/signUp/profile");
-    };
+  const navigate = useNavigate();
+  const gotoHome = () => {
+    navigate("/");
+  };
+  const gotoProfile = () => {
+    navigate("/signUp/profile");
+  };
 
-    const nameHandler = (event: React.FormEvent<HTMLInputElement>) => {
-        setName(event.currentTarget.value);
-    };
-    const birthHandler = (event: React.FormEvent<HTMLInputElement>) => {
-        setBirth(+event.currentTarget.value);
-    };
-    const phoneHandler = (event: React.FormEvent<HTMLInputElement>) => {
-        setPhone(+event.currentTarget.value);
-    };
-    const emailHandler = (event: React.FormEvent<HTMLInputElement>) => {
-        setEmail(event.currentTarget.value);
-    };
-    const domainHandler = (event: React.FormEvent<HTMLSelectElement>) => {
-        setDomain(event.currentTarget.value);
-    };
-    const pwHandler = (event: React.FormEvent<HTMLInputElement>) => {
-        setPw(event.currentTarget.value);
-    };
-    const pwCheckHandler = (event: React.FormEvent<HTMLInputElement>) => {
-        setPwCheck(event.currentTarget.value);
-    };
+  const nameHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setName(event.currentTarget.value);
+  };
+  const birthHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setBirth(+event.currentTarget.value);
+  };
+  const phoneHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setPhone(+event.currentTarget.value);
+  };
+  const emailHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+  const domainHandler = (event: React.FormEvent<HTMLSelectElement>) => {
+    setDomain(event.currentTarget.value);
+  };
+  const pwHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setPw(event.currentTarget.value);
+  };
+  const pwCheckHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setPwCheck(event.currentTarget.value);
+  };
 
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(name, birth, phone, email, pw, pwCheck);
-    };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(name, birth, phone, email, pw, pwCheck);
+  };
 
-    return (
-        <SignUpComponent>
-            <SignUpInsideBox>
-                <LogoImg src={logo} onClick={gotoHome}></LogoImg>
-                <BlackLine></BlackLine>
-                <SignUpTxt>회원가입하기</SignUpTxt>
-                <SignUpTxt2>회원가입에 필요한 정보를 입력해주세요</SignUpTxt2>
-                <form onSubmit={onSubmit}>
-                    <ExplainTxt>실명</ExplainTxt>
-                    <NameInput
-                        value={name}
-                        onChange={nameHandler}
-                        type="text"
-                        placeholder="본인 이름을 입력해주세요."
-                    />
-                    <NameTxt>
-                        한글을 사용해 입력해주세요. (특수기호, 공백 사용 불가)
-                    </NameTxt>
-                    <ExplainTxt>생년월일</ExplainTxt>
-                    <BirthInput
-                        value={birth}
-                        onChange={birthHandler}
-                        type="number"
-                        placeholder="숫자 8자리를 입력해주세요  ex) 19001201"
-                    />
-                    <BirthTxt>
-                        한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용
-                        불가)
-                    </BirthTxt>
-                    <ExplainTxt>휴대폰번호</ExplainTxt>
-                    <PhoneInput
-                        value={phone}
-                        onChange={phoneHandler}
-                        type="number"
-                        placeholder="휴대전화번호 ‘-’를 제외하고 입력"
-                    />
-                    <PhoneTxt>
-                        한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용
-                        불가)
-                    </PhoneTxt>
-                    <ExplainTxt>이메일</ExplainTxt>
-                    <EmailSignUpInput
-                        value={email}
-                        onChange={emailHandler}
-                        type="text"
-                        placeholder="이메일아이디"
-                    />
-                    <EmailSignUpSelect value={domain} onChange={domainHandler}>
-                        {selectList.map((item) => (
-                            <option value={item} key={item}>
-                                {item}
-                            </option>
-                        ))}
-                    </EmailSignUpSelect>
-                    <EmailTxt>
-                        한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용
-                        불가)
-                    </EmailTxt>
-                    <ExplainTxt>비밀번호</ExplainTxt>
-                    <PwSignUpInput
-                        value={pw}
-                        onChange={pwHandler}
-                        type="text"
-                        placeholder="비밀번호(영문/숫자/특수문자 조합 8~20자)"
-                    />
-                    <PwTxt>
-                        8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
-                    </PwTxt>
-                    <ExplainTxt>비밀번호 확인</ExplainTxt>
-                    <PwCheckInput
-                        value={pwCheck}
-                        onChange={pwCheckHandler}
-                        type="text"
-                        placeholder="비밀번호를 다시 입력해주세요"
-                    />
-                    <PwCheckTxt>비밀번호가 일치하지 않습니다.</PwCheckTxt>
-                    <SignUpBtn onClick={gotoProfile}>회원가입</SignUpBtn>
-                </form>
-            </SignUpInsideBox>
-        </SignUpComponent>
-    );
+  return (
+    <SignUpComponent>
+      <SignUpInsideBox>
+        <LogoImg src={logo} onClick={gotoHome}></LogoImg>
+        <BlackLine></BlackLine>
+        <SignUpTxt>회원가입하기</SignUpTxt>
+        <SignUpTxt2>회원가입에 필요한 정보를 입력해주세요</SignUpTxt2>
+        <form onSubmit={onSubmit}>
+          <ExplainTxt>실명</ExplainTxt>
+          <NameInput
+            value={name}
+            onChange={nameHandler}
+            type="text"
+            placeholder="본인 이름을 입력해주세요."
+          />
+          <NameTxt>
+            한글을 사용해 입력해주세요. (특수기호, 공백 사용 불가)
+          </NameTxt>
+          <ExplainTxt>생년월일</ExplainTxt>
+          <BirthInput
+            value={birth}
+            onChange={birthHandler}
+            type="number"
+            placeholder="숫자 8자리를 입력해주세요  ex) 19001201"
+          />
+          <BirthTxt>
+            한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)
+          </BirthTxt>
+          <ExplainTxt>휴대폰번호</ExplainTxt>
+          <PhoneInput
+            value={phone}
+            onChange={phoneHandler}
+            type="number"
+            placeholder="휴대전화번호 ‘-’를 제외하고 입력"
+          />
+          <PhoneTxt>
+            한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)
+          </PhoneTxt>
+          <ExplainTxt>이메일</ExplainTxt>
+          <EmailSignUpInput
+            value={email}
+            onChange={emailHandler}
+            type="text"
+            placeholder="이메일아이디"
+          />
+          <EmailSignUpSelect value={domain} onChange={domainHandler}>
+            {selectList.map((item) => (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+          </EmailSignUpSelect>
+          <EmailTxt>
+            한글과 영문 대 소문자를 사용하세요. (특수기호, 공백 사용 불가)
+          </EmailTxt>
+          <ExplainTxt>비밀번호</ExplainTxt>
+          <PwSignUpInput
+            value={pw}
+            onChange={pwHandler}
+            type="text"
+            placeholder="비밀번호(영문/숫자/특수문자 조합 8~20자)"
+          />
+          <PwTxt>8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</PwTxt>
+          <ExplainTxt>비밀번호 확인</ExplainTxt>
+          <PwCheckInput
+            value={pwCheck}
+            onChange={pwCheckHandler}
+            type="text"
+            placeholder="비밀번호를 다시 입력해주세요"
+          />
+          <PwCheckTxt>비밀번호가 일치하지 않습니다.</PwCheckTxt>
+          <SignUpBtn onClick={gotoProfile}>회원가입</SignUpBtn>
+        </form>
+      </SignUpInsideBox>
+    </SignUpComponent>
+  );
 }
 
 export default SignUpForm;
--- src/components/SignUpProfileForm.tsx
+++ src/components/SignUpProfileForm.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import {
-    UploadImg,
-    NicknameInput,
-    RegisterProfileBtn,
-    Mask,
-    BlackLine,
-    SignUpProfileComponent,
-    SignUpProfileBox,
-    LogoImg,
-    SignUpProfileTxt,
-    SignUpProfileTxt2,
-    MaskBox,
+  UploadImg,
+  NicknameInput,
+  RegisterProfileBtn,
+  Mask,
+  BlackLine,
+  SignUpProfileComponent,
+  SignUpProfileBox,
+  LogoImg,
+  SignUpProfileTxt,
+  SignUpProfileTxt2,
+  MaskBox,
 } from "./styled";
 import logo from "../img/logo.png";
 import mask from "../img/Mask group.png";
@@ -18,57 +18,52 @@ import profileUpload from "../img/profileUpload.png";
 import { useNavigate } from "react-router-dom";
 
 export default function SignUpProfileForm() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [nickname, setNickname] = useState<string>("");
+  const [nickname, setNickname] = useState<string>("");
 
-    const nicknameHandler = (event: React.FormEvent<HTMLInputElement>) => {
-        setNickname(event.currentTarget.value);
-    };
+  const nicknameHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    setNickname(event.currentTarget.value);
+  };
 
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log();
-    };
-    const upload = () => {
-        console.log("업로드 버튼 눌림");
-    };
-    const register = () => {
-        alert("회원가입 완료");
-        navigate("/login");
-    };
-    const gotoHome = () => {
-        navigate("/");
-    };
-    return (
-        <SignUpProfileComponent>
-            <SignUpProfileBox>
-                <LogoImg src={logo} onClick={gotoHome}></LogoImg>
-                <BlackLine></BlackLine>
-                <SignUpProfileTxt>프로필등록</SignUpProfileTxt>
-                <SignUpProfileTxt2>
-                    사용하실 별명과 사진을 등록해주세요
-                </SignUpProfileTxt2>
-                <form onSubmit={onSubmit}>
-                    <MaskBox>
-                        <Mask src={mask} />
-                        <UploadImg
-                            src={profileUpload}
-                            onClick={upload}
-                        ></UploadImg>
-                    </MaskBox>
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log();
+  };
+  const upload = () => {
+    console.log("업로드 버튼 눌림");
+  };
+  const register = () => {
+    alert("회원가입 완료");
+    navigate("/login");
+  };
+  const gotoHome = () => {
+    navigate("/");
+  };
+  return (
+    <SignUpProfileComponent>
+      <SignUpProfileBox>
+        <LogoImg src={logo} onClick={gotoHome}></LogoImg>
+        <BlackLine></BlackLine>
+        <SignUpProfileTxt>프로필등록</SignUpProfileTxt>
+        <SignUpProfileTxt2>
+          사용하실 별명과 사진을 등록해주세요
+        </SignUpProfileTxt2>
+        <form onSubmit={onSubmit}>
+          <MaskBox>
+            <Mask src={mask} />
+            <UploadImg src={profileUpload} onClick={upload}></UploadImg>
+          </MaskBox>
 
-                    <NicknameInput
-                        value={nickname}
-                        onChange={nicknameHandler}
-                        type="text"
-                        placeholder="닉네임"
-                    ></NicknameInput>
-                    <RegisterProfileBtn onClick={register}>
-                        등록하기
-                    </RegisterProfileBtn>
-                </form>
-            </SignUpProfileBox>
-        </SignUpProfileComponent>
-    );
+          <NicknameInput
+            value={nickname}
+            onChange={nicknameHandler}
+            type="text"
+            placeholder="닉네임"
+          ></NicknameInput>
+          <RegisterProfileBtn onClick={register}>등록하기</RegisterProfileBtn>
+        </form>
+      </SignUpProfileBox>
+    </SignUpProfileComponent>
+  );
 }
