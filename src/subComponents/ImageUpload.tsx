eb146c52441a589a82b19eadbd2f373efa03fd5c
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-11-03 19:08:05 +0900

    feat : [ImageUpload] base64로인코딩된 src 다시 디코딩하는 blob핸들링

--- src/subComponents/ImageUpload.tsx
+++ src/subComponents/ImageUpload.tsx
@@ -3,17 +3,12 @@ import imageCompression from "browser-image-compression";
 import defaulfImage from "../img/Mask group.png";
 
 function ImageUpload() {
-  const [preview, setPreview] = useState<string>(); // 인코딩한 src가 담김
+  const [preview, setPreview] = useState<string>(""); // 인코딩한 src가 담김
   const [image, setImage] = useState<File>(); // 업로드 할 이미지 파일이 담김
   const inputRef = useRef<HTMLInputElement | null>(null); //버튼 클릭 시 input 요소에 접근하기 위해 선언
   const reader = new FileReader(); //이미지파일을 base64로 인코딩 하기 위함
 
-//   이미지가 담기면 FileReader() 활용한다. 이때 readAsDataURL은 File 혹은 Blob 을 읽은 뒤 
-//   base64로 인코딩한 문자열을 FileReader 인스턴스의 result라는 속성에 담아준다.
-// FileReader.onload
-// FileReader가 성공적으로 파일을 읽어들였을 때 트리거 되는 이벤트 핸들러이다.
   useEffect(() => {
-    console.log(image);
     if (image) {
       const reader = new FileReader();
       reader.onloadend = () => {
@@ -23,26 +18,57 @@ function ImageUpload() {
     } else {
       setPreview("");
     }
-  }, [image]);
-  //버튼 클릭 시 실행되며 폼데이터에 파일을 담아 API를 호출하면 된다!
+    console.log(image);
+    console.log(preview);
+    if (preview != "") {
+      handlingDataForm(preview);
+      console.log("handlingDataForm 실행!")
+    }
+  }, [image, preview]);
+  //blob객체에 답아 통신
+  const handlingDataForm = (preview : string) => {
+    console.log(typeof preview);
+    // dataURL 값이 data:image/jpeg:base64,~~~~~~~ 이므로 ','를 기점으로 잘라서 ~~~~~인 부분만 다시 인코딩
+    const block = preview.split(";");
+    console.log(block);
+    const contentType = block[0].split(":")[1]; // In this case "image/gif"
+    console.log(contentType);
+    // get the real base64 content of the file
+    const byteString = atob(block[1].split(",")[1]); // In this case "iVBORw0KGg...."
+    console.log(byteString);
+
+    // Blob를 구성하기 위한 준비, 이 내용은 저도 잘 이해가 안가서 기술하지 않았습니다.
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([ia], {
+      type: contentType
+    });
+    const file = new File([blob], contentType);
+
+    // 위 과정을 통해 만든 image폼을 FormData에 넣어줍니다.
+    // 서버에서는 이미지를 받을 때, FormData가 아니면 받지 않도록 세팅해야합니다.
+    const formData = new FormData();
+    formData.append("image", file);
+
+    //dispatch
+  };
+  //버튼 클릭 시 실행
   const onUploadImage = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       if (!e.target.files) {
         return;
       }
-      const formData = new FormData();
-      formData.append("image", e.target.files[0]);
-      //API호출(post방식으로 formData 보내기)
       console.log(e.target.files[0]);
       actionImgCompress(e.target.files[0]); //압축함수 호출
     },
     []
   );
-
   // 이미지 압축
-  const actionImgCompress = async (fileSrc : any) => {
+  const actionImgCompress = async (fileSrc: any) => {
     console.log("압축 시작");
-
     const options = {
       maxSizeMB: 0.2,
       maxWidthOrHeight: 1920,
@@ -63,11 +89,24 @@ function ImageUpload() {
       return;
     }
     inputRef.current.click();
-  }, []);
+
+    // var block = (preview ||'').split(";");
+    // // Get the content type
+    // var contentType = block[0].split(":")[1]; // In this case "image/gif"
+    // // get the real base64 content of the file
+    // var realData = block[1].split(",")[1]; // In this case "iVBORw0KGg...."
+
+    // // Convert to blob
+    // var blob = b64toBlob(realData, contentType);
+
+    // // Create a FormData and append the file
+    // var formData = new FormData();
+    // formData.append("image", blob);
+  }, [preview]);
 
   return (
     <div>
-        {/* 미리보기할 사진 있으면 보여주고 아니면 기본 이미지 보이도록 설정 */}
+      {/* 미리보기할 사진 있으면 보여주고 아니면 기본 이미지 보이도록 설정 */}
       {preview ? (
         <img src={preview} alt={"preview"} style={{ objectFit: "cover" }} />
       ) : (
