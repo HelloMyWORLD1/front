46e6ce7a918392f5cf07122e56371130aacc7906
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-11-14 17:10:47 +0900

    feat : [MakeBlog] 블로그 생성 스타일 및 기능 구현 완료

--- src/components/MakeBlogComponent.tsx
+++ src/components/MakeBlogComponent.tsx
@@ -1,6 +1,15 @@
 import React, { useCallback } from "react";
 import logo from "../img/logo.png";
-import { HeaderComponent, LogoImg } from "./styled";
+import {
+  MakeBlogHeader,
+  LogoImg,
+  MakeBlogTable,
+  TableLine,
+  TableTr,
+  MakeBlogTitleInput,
+  CustomReactQuill,
+  MakeBlogBtn,
+} from "./styled";
 import { useNavigate } from "react-router-dom";
 import { useState, useMemo, useEffect } from "react";
 import ReactQuill from "react-quill";
@@ -11,7 +20,6 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 
 export default function MakeBlogComponent() {
-
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const onClickLogo = () => {
@@ -42,51 +50,77 @@ export default function MakeBlogComponent() {
 
   const titleHandler = (event: React.FormEvent<HTMLInputElement>) => {
     setTitle(event.currentTarget.value);
-  }
+  };
 
-//   useEffect(() => {
-//     console.log("제목",title)
-//     console.log("내용",editorValue);
-//   }, [editorValue,title]);
+  useEffect(() => {
+    console.log("제목", title);
+    console.log("내용", editorValue);
+  }, [editorValue, title]);
 
   const onSubmit = useCallback(
     (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+      event.preventDefault();
+      if (title == "" || editorValue == "" || editorValue == "<p><br></p>") {
+        alert("제목과 내용 모두 입력해주세요!");
+      } else {
         dispatch(
-            registerBlog({
-                title: title,
-                content: editorValue
-            })
-        )
-
-    },[dispatch,title,editorValue]
-  )
+          registerBlog({
+            title: title,
+            content: editorValue,
+          })
+        );
+      }
+    },
+    [dispatch, title, editorValue]
+  );
 
   return (
     <div>
-      <HeaderComponent>
-        <div></div>
-        <div>
-          <LogoImg
-            onClick={onClickLogo}
-            src={logo}
-            width={61}
-            height={35}
-            marginTop={1}
-          ></LogoImg>
-        </div>
-        <div></div>
-      </HeaderComponent>
       <form onSubmit={onSubmit}>
-        <input type="text" placeholder="제목을 입력하세요" value={title} onChange={titleHandler} ></input>
-        <ReactQuill
-          value={editorValue}
-          onChange={setEditorValue}
-          modules={modules}
-          theme="snow"
-          placeholder="내용을 입력해주세요."
-        />
-        <button>등록하기</button>
+        <MakeBlogHeader>
+          <div></div>
+          <div>
+            <LogoImg
+              onClick={onClickLogo}
+              src={logo}
+              width={50}
+              height={30}
+              marginTop={1}
+            ></LogoImg>
+          </div>
+          <div>
+            <MakeBlogBtn>등록하기</MakeBlogBtn>
+          </div>
+        </MakeBlogHeader>
+
+        <MakeBlogTable>
+          <TableTr>
+            <td>
+              <MakeBlogTitleInput
+                type="text"
+                placeholder="제목을 입력하세요"
+                value={title}
+                onChange={titleHandler}
+              ></MakeBlogTitleInput>
+            </td>
+          </TableTr>
+          <TableTr>
+            <td>
+              <TableLine></TableLine>
+            </td>
+          </TableTr>
+          <TableTr>
+            <td>
+              <CustomReactQuill
+                value={editorValue}
+                onChange={setEditorValue}
+                modules={modules}
+                theme="snow"
+                placeholder="내용을 입력해주세요."
+              />
+            </td>
+          </TableTr>
+        </MakeBlogTable>
       </form>
     </div>
   );
