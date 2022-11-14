53931a74035764f1f3125eddc3cad249e9f1666d
leeuihyun <meta212121@naver.com>
2022-11-14 14:40:37 +0900

    feat : 포트폴리오 두번째 페이지

---
+++ src/pages/MakePortFolioSecondPage.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import MakePortfolioSecondComponent from "../components/MakePortfolioSecondComponent";
+
+function MakePortFolioSecondPage() {
+  return <MakePortfolioSecondComponent />;
+}
+
+export default MakePortFolioSecondPage;
