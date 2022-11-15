4282bf57466dc04394e472fe57c34a6849885a03
leeuihyun <meta212121@naver.com>
2022-11-15 23:16:53 +0900

    feat : 포트폴리오 등록 디자인 퍼블리싱 두번째 페이지까지 완료(기능도 거의 완성)

--- src/pages/PortFolioPage.tsx
+++ src/pages/PortFolioPage.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch } from "../hooks";
+import { inquireMyInfo } from "../slices/userSlice";
 
 function PortFolioPage() {
+  const jwtToken = localStorage.getItem("jwtToken");
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (jwtToken) {
+      dispatch(inquireMyInfo());
+    }
+  }, [jwtToken]);
   return <div>포트폴리오 페이지</div>;
 }
 
