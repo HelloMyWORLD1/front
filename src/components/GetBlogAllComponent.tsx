36a557c984aa66d7998613fd43973d6e6d417a8d
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-11-18 19:39:04 +0900

    feat : [GetBlogAllComponent] 블로그 전체 조회 오프셋 기능

--- src/components/GetBlogAllComponent.tsx
+++ src/components/GetBlogAllComponent.tsx
@@ -44,12 +44,37 @@ export default function GetBlogAllComponent() {
     pageNum: 0,
   });
 
-    useEffect(() => {
-        dispatch(getBlogAll(getBlogsData)).then(
-            (res) => (setPosts(res.payload.data.blogs))
-        ) 
-      }, []);
-  
+  useEffect(() => {
+    dispatch(getBlogAll(getBlogsData)).then((res) =>
+      setPosts(res.payload.data.blogs)
+    );
+  }, []);
+  const GetBlogMinus = () => {
+    if (getBlogsData.pageNum === 0) {
+      alert("더이상 뒤로 갈수 없습니다!");
+    } else {
+      getBlogsData.pageNum = getBlogsData.pageNum - 1;
+      dispatch(getBlogAll(getBlogsData)).then((res) =>
+        setPosts(res.payload.data.blogs)
+      );
+    }
+    console.log(getBlogsData);
+  };
+
+  const GetBlogPlus = () => {
+    console.log((blogs.data.length)%5);
+    getBlogsData.pageNum = getBlogsData.pageNum + 1;
+    if((getBlogsData.pageNum) >((blogs.data.length)%5)){
+        getBlogsData.pageNum = getBlogsData.pageNum - 1;
+        alert("더이상 보여질 내용이 없습니다!")
+    }else{
+    console.log(getBlogsData);
+    dispatch(getBlogAll(getBlogsData)).then((res) =>
+      setPosts(res.payload.data.blogs)
+    );
+    }
+    
+  };
 
   const postLists: JSX.Element[] = posts.map((post) => {
     return (
@@ -98,11 +123,11 @@ export default function GetBlogAllComponent() {
       {postLists}
       <tr>
         <td>
-          <GetBlogMoreBtn>
+          <GetBlogMoreBtn onClick={GetBlogMinus}>
             <GetBlogNextImg src={arrowLeft}></GetBlogNextImg>
           </GetBlogMoreBtn>
-          <GetBlogMoreBtn>1</GetBlogMoreBtn>
-          <GetBlogMoreBtn>
+          <GetBlogMoreBtn>{getBlogsData.pageNum + 1}</GetBlogMoreBtn>
+          <GetBlogMoreBtn onClick={GetBlogPlus}>
             <GetBlogNextImg src={arrowRight}></GetBlogNextImg>
           </GetBlogMoreBtn>
         </td>
