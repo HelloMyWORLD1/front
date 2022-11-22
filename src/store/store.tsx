d78bf750fea28163b42897a64425b29c8db0fc5e
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-11-22 15:24:07 +0900

    feat : 댓글 비동기 처리 및 등록 리듀서 설정

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,12 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../slices/userSlice";
 import portFolioSlice from "../slices/portFolioSlice";
 import blogSlice from "../slices/blogSlice";
+import commentSlice from "../slices/commentSlice";
+
 // 전역으로 관리하기 위한 스토어
 const store = configureStore({
   reducer: {
     user: userSlice,
     portFolio: portFolioSlice,
-    blog: blogSlice
+    blog: blogSlice,
+    comment: commentSlice
   },
 });
 
