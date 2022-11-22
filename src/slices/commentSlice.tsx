aa1b5b38947e7f831ba003d0b4dc197dbb1dd657
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-11-22 15:36:49 +0900

    feat : 댓글 수정,삭제,조회 비동기 처리

--- src/slices/commentSlice.tsx
+++ src/slices/commentSlice.tsx
@@ -47,6 +47,62 @@ export const registerComment = createAsyncThunk(
     }
   }
 );
+//댓글 수정
+export const updateComment = createAsyncThunk(
+  "updateComment",
+  async (data: updateCommentType, { rejectWithValue }) => {
+    try {
+      axios.defaults.headers.common["Authorization"] = "";
+      const JWTTOEKN = localStorage.getItem("jwtToken");
+      axios.defaults.headers.common["Authorization"] = `Bearer ${JWTTOEKN}`;
+      const res = await axios.put(
+        `/blog/${data.blogId}/comment/${data.commentId}`,
+        data.request,
+        {
+          // withCredentials: true,
+        }
+      );
+      console.log(res.data);
+      return res.data;
+    } catch (error: any) {
+      console.log(error);
+      return rejectWithValue(error.response.data); //내부 에러처리
+    }
+  }
+);
+//댓글 삭제
+export const deleteComment = createAsyncThunk(
+  "deleteComment",
+  async (data: deleteCommentType, { rejectWithValue }) => {
+    try {
+      axios.defaults.headers.common["Authorization"] = "";
+      const JWTTOEKN = localStorage.getItem("jwtToken");
+      axios.defaults.headers.common["Authorization"] = `Bearer ${JWTTOEKN}`;
+      const res = await axios.delete(
+        `/blog/${data.blogId}/comment/${data.commentId}`
+      );
+      console.log(res.data);
+      return res.data;
+    } catch (error: any) {
+      console.log(error);
+      return rejectWithValue(error.response.data); //내부 에러처리
+    }
+  }
+);
+//댓글 조회
+export const getComment = createAsyncThunk(
+  "getComment",
+  async (data: getCommentType, { rejectWithValue }) => {
+    try {
+      const res = await axios.get(`/blog/${data.blogId}/comments`);
+      console.log(res.data);
+      return res.data;
+    } catch (error: any) {
+      console.log(error);
+      return rejectWithValue(error.response.data); //내부 에러처리
+    }
+  }
+);
 
 const commentSlice = createSlice({
   name: "comment",
@@ -72,6 +128,57 @@ const commentSlice = createSlice({
       state.registerCommentLoading = false;
       state.registerCommentDone = false;
       state.registerCommentError = action.payload;
+    },
+    //upateComment
+    [updateComment.pending.type]: (state, action: PayloadAction<object>) => {
+      state.updateCommentLoading = true;
+      state.updateCommentDone = false;
+      state.updateCommentError = null;
+    },
+    [updateComment.fulfilled.type]: (state, action: PayloadAction<object>) => {
+      state.updateCommentLoading = false;
+      state.updateCommentDone = true;
+      state.updateCommentError = null;
+      state.comment = action.payload;
+    },
+    [updateComment.rejected.type]: (state, action: PayloadAction<object>) => {
+      state.updateCommentLoading = false;
+      state.updateCommentDone = false;
+      state.updateCommentError = action.payload;
+    },
+    //deleteComment
+    [deleteComment.pending.type]: (state, action: PayloadAction<object>) => {
+      state.deleteCommentLoading = true;
+      state.deleteCommentDone = false;
+      state.deleteCommentError = null;
+    },
+    [deleteComment.fulfilled.type]: (state, action: PayloadAction<object>) => {
+      state.deleteCommentLoading = false;
+      state.deleteCommentDone = true;
+      state.deleteCommentError = null;
+      state.comment = action.payload;
+    },
+    [deleteComment.rejected.type]: (state, action: PayloadAction<object>) => {
+      state.deleteCommentLoading = false;
+      state.deleteCommentDone = false;
+      state.deleteCommentError = action.payload;
+    },
+    //getComment
+    [getComment.pending.type]: (state, action: PayloadAction<object>) => {
+      state.getCommentLoading = true;
+      state.getCommentDone = false;
+      state.getCommentError = null;
+    },
+    [getComment.fulfilled.type]: (state, action: PayloadAction<object>) => {
+      state.getCommentLoading = false;
+      state.getCommentDone = true;
+      state.getCommentError = null;
+      state.comments = action.payload;
+    },
+    [getComment.rejected.type]: (state, action: PayloadAction<object>) => {
+      state.getCommentLoading = false;
+      state.getCommentDone = false;
+      state.getCommentError = action.payload;
     },
   },
 });
