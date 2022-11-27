0e21c691ed10970577c91eaf0be7ebf8c85a0591
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-11-27 17:38:07 +0900

    feat: [getComments] 댓글 조회 성공

--- src/components/GetComments.tsx
+++ src/components/GetComments.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   GetBlogAllTable,
   GetBlogAllTr,
@@ -9,50 +9,43 @@ import {
   GetBlogAllCreated,
   GetCommentsDiv,
   GetCommentsProfile,
-  GetCommentsMoreBtn
+  GetCommentsMoreBtn,
 } from "./styled";
 import testImg from "../img/testImg.png";
-import moreBtn from "../img/moreBtn.png"
+import moreBtn from "../img/moreBtn.png";
+import { useAppDispatch } from "../hooks";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { getComment } from "../slices/commentSlice";
 
 interface Comment {
+  commentId: number;
   nickname: string;
   profile: string;
   content: string;
   createdAt: string;
 }
 export default function GetComments() {
-  const [comments, setComments] = React.useState<Comment[]>([
-    {
-      nickname: "Jaewon",
-      profile: testImg,
-      content:
-        "댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력",
-      createdAt: "2022.11.21",
-    },
-    {
-      nickname: "Jaewon",
-      profile: testImg,
-      content:
-        "댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력",
-      createdAt: "2022.11.21",
-    },
-    {
-      nickname: "Jaewon",
-      profile: testImg,
-      content:
-        "댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력",
-      createdAt: "2022.11.21",
-    },
-    {
-      nickname: "Jaewon",
-      profile: testImg,
-      content:
-        "댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력댓글입력",
-      createdAt: "2022.11.21",
-    },
-  ]);
+  const { comment,comments } = useSelector((state: RootState) => state.comment);
+  const dispatch = useAppDispatch();
+  const blogIdNum = Number(document.location.href.split("/:")[1]);
+  console.log(document.location.href.split("/")[4]);
+  const [blogId, setBlogId] = React.useState<getCommentType>({
+    blogId: blogIdNum,
+  });
+  const [commentCnt,setCommentCtn] = React.useState<number>(0);
 
-  const commentLists: JSX.Element[] = comments.map((comment) => {
+  const [detailComments, setDetailComments] = React.useState<Comment[]>([]);
+  useEffect(() => {
+    dispatch(getComment(blogId)).then((res) =>{
+        setDetailComments(res.payload.data.comments)
+        setCommentCtn(res.payload.data.length)
+    }
+    
+    );
+  }, []);
+
+  const commentLists: JSX.Element[] = detailComments.map((comment) => {
     return (
       <GetBlogAllTr>
         <td>
@@ -63,11 +56,15 @@ export default function GetComments() {
                   <GetBlogAllTitle>
                     <GetCommentsProfile
                       src={comment.profile}
-                    ></GetCommentsProfile> {comment.nickname} {comment.createdAt}
+                    ></GetCommentsProfile>{" "}
+                    {comment.nickname} {comment.createdAt}
                   </GetBlogAllTitle>
                   <div></div>
-                  <GetBlogAllCreated> 
-                  <GetCommentsMoreBtn><img src={moreBtn}></img></GetCommentsMoreBtn></GetBlogAllCreated>
+                  <GetBlogAllCreated>
+                    <GetCommentsMoreBtn>
+                      <img src={moreBtn}></img>
+                    </GetCommentsMoreBtn>
+                  </GetBlogAllCreated>
                 </GetCommentsHeaderBox>
               </tr>
               <tr>
@@ -86,7 +83,7 @@ export default function GetComments() {
     <GetBlogAllTable>
       <GetBlogAllTr>
         <td>
-          <div>댓글 0</div>
+          <div>댓글 {commentCnt}</div>
         </td>
       </GetBlogAllTr>
       {commentLists}
