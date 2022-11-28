238e3042631f05444522cb52398a7a7d90cc70a6
leeuihyun <meta212121@naver.com>
2022-11-28 18:46:36 +0900

    feat : Header 햄버거 메뉴

--- src/subComponents/Header.tsx
+++ src/subComponents/Header.tsx
@@ -9,6 +9,7 @@ import {
   HomeMenu,
   HeaderComponentBox,
   HomeMenuList,
+  HamburgerMenu,
 } from "../components/styled";
 import search from "../img/Search.png";
 import logo from "../img/logo.png";
@@ -16,8 +17,9 @@ import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../hooks";
 import { useAppSelector } from "../hooks";
 import { RootState } from "../store/store";
-import { userSliceActions } from "../slices/userSlice";
+import { deleteAccount, userSliceActions } from "../slices/userSlice";
 import { getPortFolio } from "../slices/portFolioSlice";
+import Swal from "sweetalert2";
 
 function Header() {
   const navigate = useNavigate();
@@ -68,11 +70,32 @@ function Header() {
     navigate("/");
   };
 
+  const onClickAccountDelete = () => {
+    Swal.fire({
+      title: "정말 계정을 삭제하시겠습니까?",
+      text: "삭제 후에는 복구할 수 없습니다.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(deleteAccount());
+        Swal.fire("Deleted!", "계정이 삭제되었습니다.", "success");
+      }
+    });
+  };
+
   return (
     <HeaderComponentBox>
       <HeaderComponent>
         <LinkCategory>
-          <SearchImg src={search} onClick={onClickSearch}></SearchImg>
+          <HamburgerMenu onClick={onClickSearch} show={menu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </HamburgerMenu>
           <LinkCategoryCard onClick={onClickHome}>홈</LinkCategoryCard>
           <LinkCategoryCard onClick={onClickIntro}>소개</LinkCategoryCard>
           <LinkCategoryCard onClick={onClickCommunity}>
@@ -103,12 +126,14 @@ function Header() {
       {user ? (
         <HomeMenu show={menu} logIn={user ? true : false}>
           <HomeMenuList>회원정보관리</HomeMenuList>
-          <HomeMenuList>포트폴리오</HomeMenuList>
-          <HomeMenuList>계정탈퇴</HomeMenuList>
+          <HomeMenuList onClick={onClickMakePortFolioBtn}>
+            포트폴리오
+          </HomeMenuList>
+          <HomeMenuList onClick={onClickAccountDelete}>계정탈퇴</HomeMenuList>
         </HomeMenu>
       ) : (
         <HomeMenu show={menu} logIn={user ? true : false}>
-          <HomeMenuList>로그인</HomeMenuList>
+          <HomeMenuList onClick={onClickLogIn}>로그인</HomeMenuList>
         </HomeMenu>
       )}
     </HeaderComponentBox>
