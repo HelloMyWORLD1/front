c21a104d035a5c2263d337ccb39671e7e93e8648
leeuihyun <meta212121@naver.com>
2022-12-01 22:04:34 +0900

    feat : 중복되는 훅스를 줄이기 위한 유틸함수 생성

--- src/utils/array.tsx
+++ src/utils/array.tsx
@@ -1,3 +1,53 @@
+import React, { KeyboardEvent } from "react";
+import { CheckWarnTxt } from "../components/styled";
+
 export const snsArray = {
   sns: ["instagram", "facebook"],
+};
+//{!showEducation && education && (
+//  <CheckWarnTxt>학력 입력 후 Enter을 눌러 추가하세요.</CheckWarnTxt>
+//  )}
+export const ErrorHook = (props: {
+  show: string;
+  state: string;
+  str: string;
+}) => {
+  return (
+    <div>
+      {!props.show && props.state && (
+        <CheckWarnTxt>
+          {props.str} 입력 후 Enter을 눌러 추가하세요.
+        </CheckWarnTxt>
+      )}
+    </div>
+  );
+};
+
+export const ErrorArrayHook = (props: {
+  show: string[];
+  state: string;
+  str: string;
+}) => {
+  return (
+    <div>
+      {Array.isArray(props.show) && props.show.length === 0 && props.state && (
+        <CheckWarnTxt>
+          {props.str} 입력 후 Enter을 눌러 추가하세요.
+        </CheckWarnTxt>
+      )}
+    </div>
+  );
+};
+
+interface KeyboardProps {
+  setState: (props: string) => void;
+  state: string;
+  event: KeyboardEvent<HTMLElement>;
+}
+
+export const keyEventUtil = (props: KeyboardProps): void => {
+  const target = props.event.key;
+  if (target === "Enter") {
+    props.setState(props.state);
+  }
 };
