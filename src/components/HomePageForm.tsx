133a073744d9f0c9e4db42594e39b3991e6f5ea1
leeuihyun <meta212121@naver.com>
2022-12-05 21:57:07 +0900

    refactor : 팔로우 api 변경으로 인한 수정

--- src/components/HomePageForm.tsx
+++ src/components/HomePageForm.tsx
@@ -7,15 +7,20 @@ import {
   HomePageMidTxt2,
 } from "./styled";
 import mid from "../img/Rectangle.png";
-import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../hooks";
+import {
+  getPortFolioLength,
+  getPortFolioLikeV2,
+} from "../slices/portFolioSlice";
 
 function HomePageForm() {
-  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const onClickTestButton = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("test");
-    navigate("/signUp/profile");
+    dispatch(getPortFolioLength({ field: "개발" }));
   };
-
+  const onClickTestButton2 = (e: React.MouseEvent<HTMLButtonElement>) => {
+    dispatch(getPortFolioLikeV2({ field: "개발" }));
+  };
   return (
     <div>
       <HomePageMidComponent>
@@ -26,6 +31,9 @@ function HomePageForm() {
           height="100%"
         />
         <HomePageMidTxt1>세상에 모든 가치를 그려내는 공간</HomePageMidTxt1>
+
+        <button onClick={onClickTestButton}>버튼1</button>
+        <button onClick={onClickTestButton2}>버튼2</button>
         <HomePageMidTxt2>단 하나의 유일한 포트폴리오를 만나다</HomePageMidTxt2>
       </HomePageMidComponent>
     </div>
