9c6308e3c7dde4fff5682504090d49dc41bb18f2
leeuihyun <meta212121@naver.com>
2022-12-07 00:11:29 +0900

    [del] : 홈페이지 폼 테스트용 코드 삭제

--- src/components/HomePageForm.tsx
+++ src/components/HomePageForm.tsx
@@ -7,20 +7,8 @@ import {
   HomePageMidTxt2,
 } from "./styled";
 import mid from "../img/Rectangle.png";
-import { useAppDispatch } from "../hooks";
-import {
-  getPortFolioLength,
-  getPortFolioLikeV2,
-} from "../slices/portFolioSlice";
 
 function HomePageForm() {
-  const dispatch = useAppDispatch();
-  const onClickTestButton = (e: React.MouseEvent<HTMLButtonElement>) => {
-    dispatch(getPortFolioLength({ field: "개발" }));
-  };
-  const onClickTestButton2 = (e: React.MouseEvent<HTMLButtonElement>) => {
-    dispatch(getPortFolioLikeV2({ field: "개발" }));
-  };
   return (
     <div>
       <HomePageMidComponent>
@@ -31,9 +19,6 @@ function HomePageForm() {
           height="100%"
         />
         <HomePageMidTxt1>세상에 모든 가치를 그려내는 공간</HomePageMidTxt1>
-
-        <button onClick={onClickTestButton}>버튼1</button>
-        <button onClick={onClickTestButton2}>버튼2</button>
         <HomePageMidTxt2>단 하나의 유일한 포트폴리오를 만나다</HomePageMidTxt2>
       </HomePageMidComponent>
     </div>
