9d20131a93f6469d6225326e8ca4295c16acc0bc
leeuihyun <meta212121@naver.com>
2022-12-11 01:09:33 +0900

    [feat] 개인정보 수정 페이지에서 사용하는 사용자 훅스 생성

---
+++ src/useHooks/useMyInfoUpdate.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, SetStateAction, Dispatch } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+
+function useMyInfoUpdate(
+  setNickname: Dispatch<SetStateAction<string>>,
+  setName: Dispatch<SetStateAction<string>>,
+  setBirth: Dispatch<SetStateAction<string>>,
+  setPhone: Dispatch<SetStateAction<string>>,
+  setEmail: Dispatch<SetStateAction<string>>,
+  setDomain: Dispatch<SetStateAction<string>>,
+  setField: Dispatch<SetStateAction<string>>
+) {
+  const { user } = useSelector((state: RootState) => state.user);
+
+  return useEffect(() => {
+    if (user) {
+      setNickname(user.nickname);
+      setName(user.username.split("#")[0]);
+      setBirth(user.birth);
+      setPhone(user.phone);
+      setEmail(user.email.split("@")[0]);
+      setDomain(user.email.split("@")[1]);
+      setField(user.field);
+    }
+  }, [user]);
+}
+
+export default useMyInfoUpdate;
+
+// useEffect(() => {
+//   if (user) {
+//     setNickname(user!.nickname);
+//     setName(user!.username.split("#")[0]);
+//     setBirth(user!.birth);
+//     setPhone(user!.phone);
+//     setEmail(user!.email.split("@")[0]);
+//     setDomain(user!.email.split("@")[1]);
+//     setField(user!.field);
+//   }
+// }, [user]);
